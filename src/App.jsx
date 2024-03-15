929cfe3f41835756e1055f60daaaf3cafbc599c6
Sakib <ssar96540@gmail.com>
2024-03-15 19:57:23 +0600

    created react prject with taiwind and daisyUI

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+ 
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <h1 className='text-4xl font-extrabold text-center'>hello world</h1>
+      <button className='btn btn-primary'>Click Me</button>
+    </div>
+  )
+}
+
+export default App
